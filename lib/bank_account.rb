be84796355a6518175e6dd6940e3685b5ef2b1b3
Gamerman2001 <Tro12185@gmail.com>
2019-04-04 13:52:55 -0400

    we did it again

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
+require 'pry'
+require_relative './transfer.rb'
+
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name 
+    def initialize(name)
+        @balance = 1000
+        @status = "open"
+        @name = name
+    end
+
+    def deposit(money)
+        @balance+= money
+    end 
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end 
+
+    def valid?
+         @status == "open" && @balance > 0 ? true : false 
+    end 
+
+    def close_account
+        @status = "closed"
+    end 
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
+require 'pry'
+require_relative './bank_account.rb'
+
 class Transfer
-  # your code here
+
+attr_accessor :status, :transfer_amount
+attr_reader :sender, :receiver
+
+    def initialize(sender, receiver, transfer_amount)
+      @sender = sender
+      @receiver = receiver
+      @status = "pending"
+      @transfer_amount = transfer_amount
+    end
+
+    def amount 
+      @transfer_amount
+    end
+
+    def valid?
+      receiver.valid? && sender.valid?  ? true : false
+    end
+
+    def execute_transaction
+        # sender.balance -= transfer_amount
+        # receiver.balance += transfer_amount
+        
+        if @status == "pending" && sender.balance > @transfer_amount
+        sender.deposit(-transfer_amount)
+        receiver.deposit(+transfer_amount)
+        @status = "complete"
+        else 
+          @status = "rejected"
+          "Transaction rejected. Please check your account balance."
+        end
+    end 
+
+    def reverse_transfer
+      if @status == "complete" && receiver.balance > @transfer_amount
+        receiver.deposit(-transfer_amount)
+        sender.deposit(+transfer_amount)
+        @status = "reversed"
+        else 
+          @status = "rejected"
+          "Transaction rejected. Please check your account balance."
+        end
+      # binding.pry
+    end 
+
+
 end
